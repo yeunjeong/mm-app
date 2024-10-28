32dac28c019f88e80163405e7d090f951c8cca8e
yeunjeong <araruu2536@gmail.com>
2024-10-29 00:51:45 +0900

    국밥

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,12 @@ import styles from "./Main.module.css";
 import normalImg from "./assets/images/normal.png";
 import handupImg from "./assets/images/handup.png";
 import hungryImg from "./assets/images/hungry.png";
-import foodImg from "./assets/images/food.png";
+import backImg from "./assets/images/back.png";
 
 function Main() {
   const [value, setValue] = useState(1);
   const [image, setImage] = useState(normalImg); // 현재 이미지 상태
+  const [isBack, setIsBack] = useState(false);
 
   // 밥 버튼 함수
   const handleButtonClick = () => {
@@ -30,21 +31,36 @@ function Main() {
     }
   };
 
+  const handleImgClick = () => {
+    if (isBack) {
+      updateImage(value);
+      setIsBack(false);
+    } else {
+      setImage(backImg);
+      setIsBack(true);
+    }
+  };
+
   // 게이지 바가 줄어드는 효과
   useEffect(() => {
     const interval = setInterval(() => {
       setValue((prevValue) => {
         const newValue = prevValue > 0 ? prevValue - 1 : 0; // 값이 0보다 크면 1씩 감소
+
         updateImage(newValue); // 새로운 value에 따라 이미지 업데이트
+
         return newValue;
       });
     }, 500); // 0.5초마다 감소
 
     return () => clearInterval(interval); // 컴포넌트가 언마운트될 때 interval 정리
   }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
+        <div className={styles.text}># 국밥</div>
+
         <div className={styles.gauge}>
           <div
             className={styles.fill}
@@ -56,7 +72,7 @@ function Main() {
         <img src={image} alt="normal" />
       </div>
       <div className={styles.footer}>
-        <button onClick={handleButtonClick}>밥</button>
+        <button onClick={handleButtonClick}></button>
       </div>
     </div>
   );
